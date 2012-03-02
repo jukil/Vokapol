d53b585b31ac94fd503c32f0df1da72d990fcf5d
Robert Orzanna <orschiro@googlemail.com>
2012-03-02 11:07:46 +0100

    Initial commit.

---
+++ vokapol.py
@@ -0,0 +1,16 @@
+﻿from bottle import route, run
+
+@route('/')
+def ask_vocabulary():
+    list = open('list.txt')
+    vocabulary = list.read()
+    #return vocabulary + "hallo"
+    return vocabulary, "<br><input type=\"text\"><br><input type=\"submit\" value=\"Prove\">"
+
+@post('/<vocabulary>')
+def prove_vocabulary(vocabulary):
+    
+    
+run(host='localhost', port=8080, debug=True, reloader=True)
+    
+    
